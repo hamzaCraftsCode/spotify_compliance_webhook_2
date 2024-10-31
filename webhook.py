82896437ad8fb7b8158de1312bdb5435ed8bde11
Syed Hamza <syed.hamza@data-pilot.com>
2024-10-31 18:56:10 +0500

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    description: str = None
+
 @app.post("/submit")
-async def submit_item():
+async def submit_item(item: Item):
     return {"status": 200, "message": "Request received successfully"}
