f4e890ee1889b65a5dcc7634c4611995e92767a6
Syed Hamza <syed.hamza@data-pilot.com>
2024-10-31 19:18:36 +0500

    Update submit.js

--- api/submit.js
+++ api/submit.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
-    if (req.method === 'POST') {
+    if (req.method === 'GET') {
       res.status(200).json({ status: 200, message: "Request received successfully" });
     } else {
       res.status(405).end(); // Method Not Allowed
