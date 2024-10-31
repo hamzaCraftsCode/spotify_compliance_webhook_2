7da367b9a16f10a9b8683ead49758f0526d716a5
Syed Hamza <syed.hamza@data-pilot.com>
2024-10-31 18:59:15 +0500

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -3,4 +3,7 @@ app = FastAPI()
 
 @app.post("/submit")
 async def submit_item():
-    return {"status": 200, "message": "Request received successfully"}
+    return '''
+        <h1>Connect your Shopify store</h1>
+    '''
+    # return {"status": 200, "message": "Request received successfully"}
