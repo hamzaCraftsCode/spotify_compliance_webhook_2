cb7ed559a6b7b9ff50c1d4e7bbc93568ba5c1ee1
Syed Hamza <syed.hamza@data-pilot.com>
2024-11-04 10:23:09 +0500

    Update submit.js and added back the response body

--- api/submit.js
+++ api/submit.js
@@ -1,16 +1,16 @@
-// export default function handler(req, res) {
-//     if (req.method === 'GET') {
-//       res.status(200).json({ status: 200, message: "Request received successfully" });
-//     } else {
-//       res.status(405).end(); // Method Not Allowed
-//     }
-//   }
-
 export default function handler(req, res) {
     if (req.method === 'GET') {
-      res.status(200);
+      res.status(200).json({ status: 200, message: "Request received successfully" });
     } else {
       res.status(405).end(); // Method Not Allowed
     }
   }
+
+// export default function handler(req, res) {
+//     if (req.method === 'GET') {
+//       res.status(200);
+//     } else {
+//       res.status(405).end(); // Method Not Allowed
+//     }
+//   }
   
