65bd1cc0ef484b27a82551e2032e3968020bb014
Syed Hamza <syed.hamza@data-pilot.com>
2024-11-04 10:18:29 +0500

    Update submit.js and remove response body

--- api/submit.js
+++ api/submit.js
@@ -1,6 +1,14 @@
+// export default function handler(req, res) {
+//     if (req.method === 'GET') {
+//       res.status(200).json({ status: 200, message: "Request received successfully" });
+//     } else {
+//       res.status(405).end(); // Method Not Allowed
+//     }
+//   }
+
 export default function handler(req, res) {
     if (req.method === 'GET') {
-      res.status(200).json({ status: 200, message: "Request received successfully" });
+      res.status(200);
     } else {
       res.status(405).end(); // Method Not Allowed
     }
